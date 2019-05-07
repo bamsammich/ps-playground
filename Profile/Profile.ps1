b1f5099a5a7992d73895c71f7300f5c4e8a62629
bamsammich <travis.huddleston@gmail.com>
2019-05-07 10:14:31 -0400

    Set profile auto-update using github as authority

--- Profile/Profile.ps1
+++ Profile/Profile.ps1
@@ -1,3 +1,4 @@
+#requires -RunAsAdministrator
 function Import-PSGalleryModule {
   [CmdletBinding()]
   param (
@@ -22,6 +23,46 @@ function Import-PSGalleryModule {
   }
 }
 
-$psgallery_modules = @('posh-git', 'oh-my-posh', 'Get-ChildItemColor')
+function Get-GitFile {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory, ValueFromPipeline)]
+    [ValidatePattern('https://raw\.githubusercontent\.com.*')]
+    [string]
+    $URL
+  )
 
+  process {
+    return (Invoke-WebRequest -Uri $URL).Content
+  }
+}
+
+function Initialize-Profile {
+  [CmdletBinding()]
+  param ()
+
+  process {
+    if (-not (Test-Path $profile)) {
+      New-Item $profile -Value "# Empty Profile"
+    }
+  }
+}
+$psgallery_modules = @('posh-git', 'oh-my-posh', 'Get-ChildItemColor')
+$git_ps_profile_url = 'https://raw.githubusercontent.com/bamsammich/PowerShell/master/Profile/Profile.ps1'
+
+# Ensure Profile used by this session exists
+Initialize-Profile
+
+# Import defined PSGallery Modules
 $psgallery_modules | Import-PSGalleryModule
+
+# Update local profile from github repo (if current does not match)
+$git_ps_profile = Get-GitFile $git_ps_profile_url
+$local_profile = Get-Content $profile -Raw
+if ($local_profile -ne $git_ps_profile) {
+  Write-Information "Updating local profile from github location."
+  $git_ps_profile | Out-File $profile
+}
+else {
+  Write-Information "Matched"
+}
