e8393fc9623fa198affe06156ac8c252573a312f
bamsammich <travis.huddleston@gmail.com>
2019-05-08 09:07:07 -0400

    Set host window title to contain relevant info

--- Profile/Profile.ps1
+++ Profile/Profile.ps1
@@ -47,12 +47,22 @@ function Initialize-Profile {
     }
   }
 }
+function Test-IsAdministrator {
+  $current_principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+  if ($current_principal.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+    $true
+  }
+  else {
+    $false
+  }
+}
 $psgallery_modules = [System.Collections.Generic.List[string]]::new()
 # PowerShell Development Modules
 ('Pester', 'Plaster', 'psake', 'platyPS', 'PowerShellBuild', 'Stucco') | ForEach-Object { $psgallery_modules.Add($_) }
 # PowerShell Commandline Quality of Life Modules
 ('posh-git', 'oh-my-posh', 'Get-ChildItemColor') | ForEach-Object { $psgallery_modules.Add($_) }
-if (-not (Get-Module 'PSReadline')) {$psgallery_modules.Add('PSReadline')}
+if (-not (Get-Module 'PSReadline')) { $psgallery_modules.Add('PSReadline') }
 
 $git_ps_profile_url = 'https://raw.githubusercontent.com/bamsammich/PowerShell/master/Profile/Profile.ps1'
 $git_ps_theme_url = 'https://raw.githubusercontent.com/bamsammich/PowerShell/master/Themes/Paradox_custom.psm1'
@@ -62,6 +72,16 @@ Initialize-Profile
 
 # Import defined PSGallery Modules
 $psgallery_modules | Import-PSGalleryModule
+
+$host_title = [ordered]@{
+  'Elevation' = $(if (Test-IsAdministrator) { "Admin" } else { "Non-Admin" });
+  'Version'   = $PSVersionTable.PSVersion.ToString();
+  'Edition'   = $PSVersionTable.PSEdition;
+  'Session'   = "$env:USERNAME@$env:COMPUTERNAME.$env:USERDOMAIN".ToLower();
+}
+
+# Set the Window Title
+$host.ui.RawUI.WindowTitle = "PowerShell [ $($host_title.Values -join ' | ') ]"
 
 # Set Theme
 $theme_name = ($git_ps_theme_url -split '/' | Select-Object -Last 1).Trim()
